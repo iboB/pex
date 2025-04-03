527b70e6a892d02d668071f57b7f5d45e74b9dfd
Borislav Stanimirov <b@ibob.bg>
2025-04-03 21:01:49 +0300

    disable test on gcc with O0

--- test/t-coro-stack.cpp
+++ test/t-coro-stack.cpp
@@ -6,6 +6,26 @@
 #include <pex/co_spawn.hpp>
 #include <doctest/doctest.h>
 
+// GCC does not implement symmetric transfer with O0
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100897
+// https://github.com/iboB/gcc-coro-stack-overflow
+
+#if !defined(__GNUC__)
+// enable if not gnuc
+#define ENABLE 1
+#elif defined(__clang__)
+// enable with clang
+#define ENABLE 1
+// enable with O1 or more
+#elif defined(__OPTIMIZE__) && __OPTIMIZE__ > 0
+#define ENABLE 1
+#else
+// disable with gcc and O0
+#define ENABLE 0
+#endif
+
+#if ENABLE
+
 #define B_STACKTRACE_IMPL
 #include <b_stacktrace.h>
 
@@ -32,3 +52,5 @@ TEST_CASE("stack flatten") {
     co_spawn(ctx, stack_flatten_test());
     ctx.run();
 }
+
+#endif
